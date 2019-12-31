7d6af9644913122ad01c2c6ad4ed497cf6717545
Hal Le <hle@freebalance.com>
2019-12-31 07:42:06 -0500

    commit locally

--- DjangoLearn/forms.py
+++ DjangoLearn/forms.py
@@ -2,7 +2,36 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full name",
+                "id":"form_full_name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your email",
+                "id": "form_email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your content",
+                "id": "form_content"
+            }
+        )
+    )
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to include a gmail address")
+        return email
